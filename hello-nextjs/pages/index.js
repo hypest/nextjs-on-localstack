4b975826b4494421c01a4d3072936e295e878910
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2025-12-02 23:53:30 +0000

    test commit

--- hello-nextjs/pages/index.js
+++ hello-nextjs/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem" }}>
-      <h1>Hello World! Production!</h1>
+      <h1>Hello World! Production2!</h1>
       <p>Next.js app deployed to LocalStack S3 via Terraform.</p>
     </main>
   );
