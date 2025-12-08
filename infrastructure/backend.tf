f65b6190a103f99898a3a17bb18d3341dd32947c
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2025-12-08 23:47:20 +0000

    Update CI script to set AWS_ENDPOINT_URL for Terraform commands

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -3,13 +3,11 @@ terraform {
     bucket                      = "terraform-state-devcontainer-localstack"
     key                         = "terraform.tfstate"
     region                      = "us-east-1"
-    endpoints = {
-      s3 = "http://localhost:4566"
-    }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
     use_path_style              = true
     # Use default AWS credentials (test/test for LocalStack)
+    # Endpoint configured via AWS_ENDPOINT_URL environment variable
   }
 }
