17c7117f6f022593fd867c22463446de6add2e2c
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2025-12-09 00:23:40 +0000

    Get to LocalStack via Docker for terraform's backend S3

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
     endpoints = {
-      s3 = "http://localhost:4566"
+      s3 = "http://host.docker.internal:4566"
     }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
