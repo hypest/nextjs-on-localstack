e6cfb53674f0f00741740a271365c3e10f222e70
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2025-12-30 20:57:49 +0000

    Fix invalid backend configuration for Terraform 1.6+

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -3,13 +3,12 @@ terraform {
     # bucket is set dynamically via -backend-config
     key    = "terraform.tfstate"
     region = "us-east-1"
-    endpoints = {
-      s3 = "http://host.docker.internal:4566"
-    }
+    
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
     use_path_style              = true
     # Use default AWS credentials (test/test for LocalStack)
+    # Endpoints are handled via AWS_ENDPOINT_URL environment variable (Terraform 1.6+)
   }
 }
